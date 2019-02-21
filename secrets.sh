48f88f6823a49013e5fe048f402e4873c11d4e21
Henrik Johansson <henrik.johansson712@gmail.com>
2019-02-21 17:05:55 +0100

    Commented out cd command

--- secrets.sh
+++ secrets.sh
@@ -1,3 +1,3 @@
-cd $HOME/Projects/synapse/contrib/docker/files
+# cd $HOME/Projects/synapse/contrib/docker/files
 
 openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout localhost.tls.key -out localhost.tls.crt -subj "/CN=localhost" -days 3650
