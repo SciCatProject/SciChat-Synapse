69e6a2d4ad30e5e3c7575ebcfa211ade11d3c3dd
Henrik Johansson <henrik.johansson712@gmail.com>
2019-02-21 10:44:24 +0100

    Updated secrets

--- secrets.sh
+++ secrets.sh
@@ -1,3 +1,3 @@
 cd $HOME/Projects/synapse/contrib/docker/files
 
-openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout scichat.synapse.se.tls.key -out scichat.synapse.se.tls.crt -subj "/CN=scichat.synapse.se" -days 3650
+openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout localhost.tls.key -out localhost.tls.crt -subj "/CN=localhost" -days 3650
