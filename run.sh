b7750cfd9e2a9dc3166d6bbf32ae0c57d7efd608
Henrik Johansson <henrik.johansson712@gmail.com>
2019-03-12 15:03:25 +0100

    Fixed errors in run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 git clone https://github.com/matrix-org/synapse.git
 cd synapse/contrib/docker/
-sed -i '18s/.*/- SYNAPSE_SERVER_NAME=${HOSTNAME}' docker-compose.yml
+sed -ie "18s/.*/      - SYNAPSE_SERVER_NAME=${HOSTNAME}/" docker-compose.yml
+sed -ie "37s/.*/    # labels:/" docker-compose.yml
+sed -ie "38s/.*/      # - traefik.enable=true/" docker-compose.yml
+sed -ie "39s/.*/      # - traefik.frontend.rule=Host:my.matrix.Host/" docker-compose.yml
+sed -ie "40s/.*/      # - trafeik.port=8008/" docker-compose.yml
 mkdir ./files
 cd ./files
 wget https://raw.githubusercontent.com/SciCatProject/SciChat-Synapse/master/secrets.sh
+chmod +x ./secrets.sh
 ./secrets.sh
 cd ..
 docker-compose up
