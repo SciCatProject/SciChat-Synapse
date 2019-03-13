ab7dc3d073c16853469019ceab96f8ad07d62a56
Henrik Johansson <henrik.johansson712@gmail.com>
2019-03-13 15:12:23 +0100

    Added insertion of environment variable in docker-compose.yml to limit RAM usage of synapse

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 git clone https://github.com/matrix-org/synapse.git
 cd synapse/contrib/docker/
 sed -i.example -e "18s/.*/      - SYNAPSE_SERVER_NAME=${HOSTNAME}/" -e "37s/.*/    # labels:/" -e "38s/.*/      # - traefik.enable=true/" -e "39s/.*/      # - traefik.frontend.rule=Host:my.matrix.Host/" -e "40s/.*/      # - trafeik.port=8008/" docker-compose.yml
+# Line below is used to limit RAM usage by decreasing the cache
+sed -i.example -e "21a\      - SYNAPSE_CACHE_FACTOR=0.5" docker-compose.yml
 mkdir ./files
 cd ./files
 wget https://raw.githubusercontent.com/SciCatProject/SciChat-Synapse/master/secrets.sh
