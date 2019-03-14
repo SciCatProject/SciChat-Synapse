1fe77ce1b81cd7b7c6c490792657c69edda96d91
Henrik Johansson <henrik.johansson712@gmail.com>
2019-03-14 09:57:51 +0100

    Script can now also be used to run synapse when already cloned

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
-git clone https://github.com/matrix-org/synapse.git
-cd synapse/contrib/docker/
-sed -i.example -e "18s/.*/      - SYNAPSE_SERVER_NAME=${HOSTNAME}/" -e "37s/.*/    # labels:/" -e "38s/.*/      # - traefik.enable=true/" -e "39s/.*/      # - traefik.frontend.rule=Host:my.matrix.Host/" -e "40s/.*/      # - trafeik.port=8008/" docker-compose.yml
-# Line below is used to limit RAM usage by decreasing the cache
-sed -i.example -e "21a\      - SYNAPSE_CACHE_FACTOR=0.5" docker-compose.yml
-mkdir ./files
-cd ./files
-wget https://raw.githubusercontent.com/SciCatProject/SciChat-Synapse/master/secrets.sh
-chmod +x ./secrets.sh
-./secrets.sh
-cd ..
-docker-compose up
+if [ -d "synapse" ]; then
+  cd ./synapse/contrib/docker/
+  docker-compose up
+else
+  git clone https://github.com/matrix-org/synapse.git
+  cd ./synapse/contrib/docker/
+  sed -i.example -e "18s/.*/      - SYNAPSE_SERVER_NAME=${HOSTNAME}/" -e "37s/.*/    # labels:/" -e "38s/.*/      # - traefik.enable=true/" -e "39s/.*/      # - traefik.frontend.rule=Host:my.matrix.Host/" -e "40s/.*/      # - trafeik.port=8008/" docker-compose.yml
+  sed -i.example -e "21a\      - SYNAPSE_CACHE_FACTOR=0.5" docker-compose.yml
+  mkdir ./files
+  cd ./files
+  wget https://raw.githubusercontent.com/SciCatProject/SciChat-Synapse/master/secrets.sh
+  chmod +x ./secrets.sh
+  ./secrets.sh
+  cd ..
+  docker-compose up
+fi
